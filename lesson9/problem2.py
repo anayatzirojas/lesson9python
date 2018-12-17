7361d2d8bc8aba2fd3e100ade6844bf6a5262170
anayatzirojas <44092257+anayatzirojas@users.noreply.github.com>
2018-12-17 12:47:41 -0500

    first commit

---
+++ lesson9/problem2.py
@@ -0,0 +1,12 @@
+print('-' *60)
+print('Locker Bot:')
+print()
+password = input('What is the password?')
+if password == 'yaya':
+	print('Password is gucci!')
+	print('Locker opening!')
+else:
+	print('Password invalid...enabling laser defense system,')
+	print('Pew Pew Pew')
+	print('Pew Pew Pew')
+print('-'*60)
---
+++ lesson9/problem3.py
@@ -0,0 +1,11 @@
+print('-' *60)
+print('Alien Greeter Bot:')
+print()
+planet= input('What planet are you from?')
+if planet == 'Earth':
+	print('Nasa is searching the data!!')
+else:
+	print('Nasa has not found any results.')
+	print('Checking once more.')
+	print('NVM we found the folder for Earth!')
+print('-'*60)
